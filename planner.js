14b855a00f6eb64774d0dbca50e4a005a7f830c7
Kashif Peshimam <kashifpeshimam@Kashifs-MacBook-Pro.local>
2022-06-20 08:04:29 -0400

    button function

--- planner.js
+++ planner.js
@@ -1,1 +1,14 @@
 // Planner code
+'use strict';
+
+window.onload = main;
+
+const entries = [];
+
+function main() {
+    document.getElementById("add-btn").onclick = addEntry;
+}
+
+function addEntry() {
+    
+}
