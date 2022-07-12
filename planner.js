4eec703b8470d18e62e7b884246c802bed07e355
Kashif Peshimam <kashif.peshimam9@gmail.com>
2022-07-12 15:22:57 -0400

    red to lighter red

--- planner.js
+++ planner.js
@@ -93,7 +93,7 @@ function addEntry(event) {
 function displayChart() {
     let xValues = []
     let yValues = []
-    let barColors = ["orange", "skyblue", "pink", "rgb(200, 127, 224)", "lightgreen", "red"];
+    let barColors = ["orange", "skyblue", "pink", "rgb(200, 127, 224)", "lightgreen", "rgb(248, 105, 105)"];
 
     let types = document.getElementsByClassName("activity-type");
 
